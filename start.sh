794bbe3717178c96fe084dcbbfaed099721223cc
aihe <aihe.ah@alibaba-inc.com>
2023-07-15 17:09:02 +0800

    启动参数

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /lotus/stable-diffusion-webui/
 
-./webui.sh  --listen  --hide-ui-dir-config  -theme=dark --no-gradio-queue 1>>logs/webui.log 2>&1
+./webui.sh  --listen  --hide-ui-dir-config  --theme=dark --no-gradio-queue 1>>logs/webui.log 2>&1
 
